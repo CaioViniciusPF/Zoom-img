c6802f3eedd1ce1d229b5c740403a77c37e38341
caiopessoa145@gmail.com <caiopessoa145@gmail.com>
2024-04-27 12:41:08 -0300

    limitacao da imagem implementacao

--- js/javascript.js
+++ js/javascript.js
@@ -44,52 +44,84 @@ const leftButton = document.getElementById('left');
 const upButton = document.getElementById('up');
 const downButton = document.getElementById('down');
 const rightButton = document.getElementById('right');
+const container = document.querySelector('.container');
 
 let scale = 1; // Inicialmente, o zoom é 1x
 let translateX = 0; // Posição horizontal inicial
 let translateY = 0; // Posição vertical inicial
-
+let qtdZoom=0;
 // Função para aplicar transformações na imagem
 function updateTransform() {
-  zoomImage.style.transform = `scale(${scale}) translate(${translateX}px, ${translateY}px)`;
+  const containerWidth = container.offsetWidth;
+  const containerHeight = container.offsetHeight;
+  const imageWidth = zoomImage.offsetWidth * scale;
+  const imageHeight = zoomImage.offsetHeight * scale;
+  
+  // Restrições de movimento para a esquerda e para a direita
+  if (translateX > 0) {
+    translateX = 0;
+    
+  } else if (translateX < -qtdZoom*10 ){
+    translateX = -qtdZoom*10 ;
+    ;
+
+  }
+  
+  // Restrições de movimento para cima e para baixo
+  if (translateY > 0) {
+    translateY = 0;
+  } else if (translateY < -qtdZoom*10) {
+    translateY = -qtdZoom*10;
+  }
+  console.log(imageWidth);
+  console.log(translateY);
+  console.log(translateX)
+  console.log("Zoom: " + qtdZoom);
+  zoomImage.style.transform = `scale(${scale})`;
+  zoomImage.style.left=`${translateX}%`;
+  zoomImage.style.top=`${translateY}%`;
 }
 
 // Event listener para o botão de zoom in
 zoomOutButton.addEventListener('click', () => {
-  scale += 0.1; // Aumenta o zoom em 0.1x
+
+  scale += 0.2; // Aumenta o zoom em 0.1x\
+  qtdZoom+=1;
   updateTransform(); 
 });
 
 // Event listener para o botão de zoom out
 zoomInButton.addEventListener('click', () => {
-  scale -= 0.1;
+  scale -= 0.2;
+  qtdZoom-=1;
   // Não permita zoom negativo
-  if (scale < 0.1) {
-    scale = 0.1;
+  if (scale < 1) {
+    qtdZoom=0;
+    scale = 1;
   }
   updateTransform(); 
 });
 
 // Event listener para o botão de mover para a esquerda
 leftButton.addEventListener('click', () => {
-  translateX -= 10; 
+  translateX -= 1;
   updateTransform(); 
 });
 
 // Event listener para o botão de mover para cima
 upButton.addEventListener('click', () => {
-  translateY -= 10; 
+  translateY -= 1; 
   updateTransform(); 
 });
 
 // Event listener para o botão de mover para baixo
 downButton.addEventListener('click', () => {
-  translateY += 10; 
+  translateY += 1; 
   updateTransform(); 
 });
 
 // Event listener para o botão de mover para a direita
 rightButton.addEventListener('click', () => {
-  translateX += 10; 
+  translateX += 1; 
   updateTransform(); 
 });
